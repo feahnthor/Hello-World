b639f6c042bdd9183472f70dd32483ec2c5b3b79
Henry Feahn <h.feahn@acilab.com>
2021-03-24 14:08:36 -0500

    first commit

---
+++ get prod names.py
@@ -0,0 +1,187 @@
+from bs4 import BeautifulSoup as get_soup
+import requests
+from webdriver_manager.chrome import ChromeDriverManager #https://github.com/SergeyPirogov/webdriver_manager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium import webdriver
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from credentials import Credentials
+import re, os, logging
+from time import sleep
+"""
+Code adapted from "Project article by Michael Haephrati" alexa.py
+1. selecting an element using driver 
+    https://stackoverflow.com/questions/25580569/get-value-of-an-input-box-using-selenium-python
+2. Webdriver commands/methods https://selenium-python.readthedocs.io/api.html
+
+Issues: requests does not get the entire dom for imagebuggy admin, so we need to get using selenium
+
+Goals 
+1. Get Product names and ids
+2. Get designer names
+"""
+
+logger = logging.getLogger("backgroundtown")
+formatter = logging.Formatter("%(asctime)s;%(levelname)s    %(message)s")
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.DEBUG)
+stream_handler.setFormatter(formatter)
+file_handler = logging.FileHandler(filename="alexa.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
+logger.setLevel(logging.DEBUG)
+
+
+# url = 'https://backgroundtown.com/backgroundtown/CA/Admin/StaticPDFProduct/Create'
+
+class WindowsInhibitor:
+    ES_CONTINUOUS = 0x80000000
+    ES_SYSTEM_REQUIRED = 0x00000001
+
+    def __init__(self):
+        pass
+
+    def inhibit(self):
+        import ctypes
+        print("Preventing Windows from going to sleep.")
+        ctypes.windll.kernel32.SetThreadExecutionState(
+            WindowsInhibitor.ES_CONTINUOUS | \
+            WindowsInhibitor.ES_SYSTEM_REQUIRED)
+
+    def uninhibit(self):
+        import ctypes
+        print("Allowing Windows to go to sleep.")
+        ctypes.windll.kernel32.SetThreadExecutionState(
+            WindowsInhibitor.ES_CONTINUOUS)
+
+# submit = driver.find_element_by_class_name('loginbutton')
+# submit.click()
+
+def init_driver():
+  logger.info("Starting chromedriver")
+  chrome_options = Options()
+  # use local data directory
+  # headless mode can't be enabled since then amazon shows captcha
+  
+  chrome_options.add_argument("user-data-dir=selenium") 
+  chrome_options.add_argument("start-maximized")
+  chrome_options.add_argument("--disable-infobars")
+  chrome_options.add_argument('--disable-gpu')  
+  # chrome_options.add_argument('user-data-dir=selenium')  
+  chrome_options.add_argument('--remote-debugging-port=4444')
+  # chrome_options.add_argument('--no-sandbox')
+  chrome_options.add_argument('--disable-dev-shm-usage')
+  chrome_options.add_argument("--mute-audio")
+  
+  try:
+    driver = webdriver.Chrome(ChromeDriverManager().install(),
+      options = chrome_options, service_log_path='NUL')
+  except ValueError:
+    logger.critical("Error opening Chrome. Chrome is not installed?")
+    exit(1)
+  driver.implicitly_wait(10)
+  return driver
+
+def url_login(driver,url):
+  driver.implicitly_wait(5)
+  logger.info(f"GET test 1 {url}")
+  # get main page
+  driver.get(url)
+  # sleep(4)
+  url = driver.current_url
+  # if site asks for signin, it will redirect to a page with signin in url
+  if 'login' in url:
+    logger.info("Got login page: logging in...")
+    # find email field
+    # WebDriverWait waits until elements appear on the page
+    # so it prevents script from failing in case page is still being loaded
+    # Also if script fails to find the elements (which should not happen
+    # but happens if your internet connection fails)
+    # it is possible to catch TimeOutError and loop the script, so it will
+    # repeat.
+    check_field = WebDriverWait(driver, 30).until(
+      EC.presence_of_element_located((By.ID, 'LoginEmail')))
+    email_field = driver.find_element_by_id('LoginEmail')
+    email_field.clear()
+    # type email
+    email_field.send_keys(Credentials.email)
+    check_field = WebDriverWait(driver, 30).until(
+      EC.presence_of_element_located((By.ID, 'LoginPassword')))
+    # find password field
+    password_field = driver.find_element_by_id('LoginPassword')
+    password_field.clear()
+    # type password
+    password_field.send_keys(Credentials.password)
+    # find submit button, submit
+    check_field = WebDriverWait(driver, 30).until(
+      EC.presence_of_element_located((By.CLASS_NAME, 'loginbutton')))
+    submit = driver.find_element_by_class_name('loginbutton')
+    submit.click()
+    driver.get('https://backgroundtown.com/Admin/Product/Edit/1039')
+  driver.get('https://backgroundtown.com/Admin/Product/Edit/1039')
+
+  # fetchData(driver)
+def fetchData(driver):
+  count = 0
+  # product_id = ''
+  # product_url = 'https://backgroundtown.com/Admin/Product/Edit/'
+  product_list = driver.find_elements_by_css_selector('#ProductVariants option')
+
+
+  cookies = driver.get_cookies()
+  # print(cookies)
+  ses = requests.Session()
+  ses.headers = {'User-Agent':
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
+  '(KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.63'}
+
+  # pass the cookies generated dfrom the browser to the session
+  c = [ses.cookies.set(c['name'], c['value']) for c in cookies]
+
+  
+  for foo in product_list:
+    product_id = foo.get_attribute('value')
+    product_url = f'https://backgroundtown.com/Admin/Product/Edit/{product_id}'
+    # print(foo.text, foo.get_attribute('value'))
+    count +=1
+    logger.info(f'TOTAL: {count}')
+    print(product_url)
+    res = ses.get(product_url)
+    status = res.status_code
+    soup = get_soup(res.content, 'html.parser')
+    logger.info(f'STATUS CODE: {status}')
+    copy_btn = soup.select('#copyproduct')
+    
+    designer = soup.select('#product-edit tbody tr td')
+    
+    ##prduct-edit tbody tr td
+    title = soup.title.text
+    if status == 200:
+      logger.info(f'Title: {title}   Button: {copy_btn}  De: {designer}')
+
+def main():
+  sys_sleep = None
+  sys_sleep = WindowsInhibitor()
+  logger.info('System inhibited')
+  sys_sleep.inhibit()
+  
+  url = 'https://backgroundtown.com/backgroundtown/CA/Admin/StaticPDFProduct/Create'
+  #start driver
+  driver = init_driver()
+
+  while True:
+    try:
+      url_login(driver, url)
+      break
+    except TimeoutException:
+      # catch broken connection
+      logger.critical("Timeout exception. No internet connection? "
+      "Retrying...")
+      sleep(10)
+      continue
+if __name__ == '__main__':
+  main()
